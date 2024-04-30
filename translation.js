77089427e3b71beab1b4bd12052b0638c1e0b05a
osmzduck <168301714+osmzduck@users.noreply.github.com>
2024-04-30 16:06:18 -0700

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,8 +1,15 @@
-
 const translatorLink = document.getElementById('translator-link');
 
 translatorLink.addEventListener('click', function(e) {
     e.preventDefault();
+
+    const translateAnimation = document.createElement('div');
+    translateAnimation.classList.add('translate-animation');
+    document.body.appendChild(translateAnimation);
+
+    setTimeout(() => {
+        translateAnimation.classList.add('active');
+    }, 100);
 
     setTimeout(() => {
         window.location.href = translatorLink.getAttribute('href');
